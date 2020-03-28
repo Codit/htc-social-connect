2069031fa882682f827b253a30d209c2ec781460
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-03-28 15:53:39 +0100

    Persist box information in Table Storage (#60)
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/CommunicationApi/Interfaces/IBoxStore.cs
+++ src/CommunicationApi/Interfaces/IBoxStore.cs
@@ -5,7 +5,7 @@ namespace CommunicationApi.Interfaces
 {
     public interface IBoxStore
     {
-        Task<ActivatedDevice> Get(string boxId);
-        Task Add(string boxId, ActivatedDevice activatedDevice);
+        Task<Models.ActivatedDevice> Get(string boxId);
+        Task Add(string boxId, Models.ActivatedDevice activatedDevice);
     }
 }
---
+++ src/CommunicationApi/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CommunicationApi.Contracts.v1;
+
+namespace CommunicationApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BoxStatus, Models.BoxStatus>();
+            CreateMap<ActivatedDevice, Models.ActivatedDevice>();
+            CreateMap<Models.BoxStatus, BoxStatus>();
+            CreateMap<Models.ActivatedDevice, ActivatedDevice>();
+        }
+    }
+}
