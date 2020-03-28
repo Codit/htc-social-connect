87b0f4f15bb228e5154416779ec60ccc1e42338e
Sam <sam.vanhoutte@marchitec.be>
2020-03-28 13:57:44 +0100

    implement BlobImageMediaServiceProvider.cs

--- src/CommunicationApi/Services/Blobstorage/BlobImageMediaServiceProvider.cs
+++ src/CommunicationApi/Services/Blobstorage/BlobImageMediaServiceProvider.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 using CommunicationApi.Interfaces;
 using CommunicationApi.Models;
 using Flurl.Http;
 using GuardNet;
 using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace CommunicationApi.Services.Blobstorage
 {
@@ -16,6 +20,7 @@ namespace CommunicationApi.Services.Blobstorage
     {
         private BlobServiceClient _blobServiceClient;
         private StorageSettings _storageSettings;
+        private UserDelegationKey _delegationKey;
 
         public BlobImageMediaServiceProvider(IOptions<StorageSettings> storageSettings)
         {
@@ -40,13 +45,20 @@ namespace CommunicationApi.Services.Blobstorage
             }
         }
 
-
-        public Task<IEnumerable<MediaItem>> GetItems(string tenantId)
+        public async Task<IEnumerable<MediaItem>> GetItems(string tenantId)
         {
-            // TODO : joachim : retrieve sas tokens here
-            throw new NotImplementedException();
-        }
+            var blobContainer = StorageClient.GetBlobContainerClient(tenantId);
 
+            var blobs = blobContainer.GetBlobs();
+
+            var response = new List<MediaItem>();
+            foreach (var blobItem in blobs)
+            {
+                response.Add(await GetMediaItem(tenantId, blobItem));
+            }
+
+            return response;
+        }
         public async Task PersistMediaFile(UserInfo userInfo, string mediaUrl)
         {
             // download image to stream
@@ -55,6 +67,7 @@ namespace CommunicationApi.Services.Blobstorage
             // Create the container and return a container client object
             var container = StorageClient.GetBlobContainerClient(userInfo.TenantInfo.Name.ToLower());
             await container.CreateIfNotExistsAsync(PublicAccessType.None);
+
             // TODO : check extension based on media type
             string extension = "jpg";
             await container.UploadBlobAsync(Guid.NewGuid().ToString("N") + "." + extension, mediaStream);
@@ -64,5 +77,56 @@ namespace CommunicationApi.Services.Blobstorage
         {
             throw new NotImplementedException();
         }
+        
+        private async Task<string> GenerateSasUri(BlobItem blob, string tenant)
+        {
+            // Create a SAS token that's valid for one hour.
+            var sasBuilder = new BlobSasBuilder()
+            {
+                BlobContainerName = tenant,
+                BlobName = blob.Name,
+                Resource = "b",
+                StartsOn = DateTimeOffset.UtcNow,
+                ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+            };
+
+            // Specify read permissions for the SAS.
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+            // Use the key to get the SAS token.
+            if (_delegationKey == null)
+            {
+                _delegationKey = await _blobServiceClient.GetUserDelegationKeyAsync(DateTimeOffset.UtcNow,
+                    DateTimeOffset.UtcNow.AddDays(7));
+            }
+            var sasToken = sasBuilder.ToSasQueryParameters(_delegationKey, _storageSettings.AccountName).ToString();
+
+            // Construct the full URI, including the SAS token.
+            var fullUri = new UriBuilder()
+            {
+                Scheme = "https",
+                Host = $"{_storageSettings.AccountName}.blob.core.windows.net",
+                Path = $"{tenant}/{blob.Name}",
+                Query = sasToken
+            };
+            return fullUri.ToString();
+        }
+
+        
+
+        private async Task<MediaItem> GetMediaItem(string tenantId, BlobItem blobItem)
+        {
+            var mediaItem = new MediaItem
+            {
+                MediaUrl = await GenerateSasUri(blobItem, tenantId), 
+                MediaType = MediaType.Image
+            };
+            if (blobItem.Properties.LastModified != null)
+                mediaItem.Timestamp = blobItem.Properties.LastModified.Value;
+            mediaItem.UserName = blobItem.Metadata.ContainsKey("user") ? blobItem.Metadata["user"] : "Onbekend";
+            return mediaItem;
+        }
+
+
     }
 }
