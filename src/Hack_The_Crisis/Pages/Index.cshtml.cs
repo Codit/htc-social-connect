04f58dbab592d4a309f083406afeec274292faa8
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-03-30 14:22:39 +0200

    Provide privacy statement and make API URL configurable

--- src/Hack_The_Crisis/Pages/Index.cshtml.cs
+++ src/Hack_The_Crisis/Pages/Index.cshtml.cs
@@ -1,75 +1,64 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.IO;
-using Arcus.Security.Core;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Microsoft.AspNetCore.Html;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using Newtonsoft.Json.Linq;
-using Microsoft.AspNetCore.Mvc;
 using System.Threading;
+using System.Threading.Tasks;
+using Arcus.Security.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace Hack_The_Crisis.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         private readonly ISecretProvider _secretProvider;
 
-
-      
-        private static readonly HttpClient httpClient = new HttpClient();
-
-
-        private string boxId;
-        private string activationCode;
-        private string boxStatus;
-
-        public string BoxId { get => boxId; set => boxId = value; }
-        public string ActivationCode { get => activationCode; set => activationCode = value; }
-        public string BoxStatus { get => boxStatus; set => boxStatus = value; }
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private string ApiUrl => _configuration["API_URL"];
+        public string BoxId { get; set; }
+        public string ActivationCode { get; set; }
+        public string BoxStatus { get; set; }
 
         private static int initBlobCount;
         private static int initTxtCount;
 
-        public IndexModel(ISecretProvider secretProvider)
+        public IndexModel(ISecretProvider secretProvider, IConfiguration configuration)
         {
+            _configuration = configuration;
             _secretProvider = secretProvider;
-
         }
-
-       
-       
 
         private async Task<JObject> RegisterNewBox()
         {
-            var response = await httpClient.PostAsync("https://codit-htc.azurewebsites.net/api/v1/box/new", null);
+            var response = await HttpClient.PostAsync($"{ApiUrl}/api/v1/box/new", null);
             var responseString = JObject.Parse(await response.Content.ReadAsStringAsync());
             return responseString;
         }
 
         private async Task<JObject> GetBoxStatus()
         {
-           
-            var response = await httpClient.GetAsync("https://codit-htc.azurewebsites.net/api/v1/box/status?boxid="+BoxId);
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/box/status?boxid=" + BoxId);
             var responseString = JObject.Parse(await response.Content.ReadAsStringAsync());
             return responseString;
         }
-        public  JsonResult OnGetGetData()
+
+        public JsonResult OnGetGetData()
         {
-           
-            return new JsonResult(initBlobCount+";"+ initTxtCount);
+            return new JsonResult(initBlobCount + ";" + initTxtCount);
         }
+
         public async Task OnGet()
         {
             initBlobCount = -1;
             initTxtCount = -1;
             var apiKey = await _secretProvider.GetSecretAsync("HTC-API-Key");
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.Add("x-api-key", apiKey.Value);
-            
+            HttpClient.DefaultRequestHeaders.Clear();
+            HttpClient.DefaultRequestHeaders.Add("x-api-key", apiKey.Value);
+
             var autoEvent = new AutoResetEvent(false);
             Timer _tm = new Timer(refreshData, autoEvent, 20000, 20000);
 
@@ -79,14 +68,11 @@ namespace Hack_The_Crisis.Pages
             string cookieActivationCode = Request.Cookies["activationCode"];
 
             if (Request.Query.ContainsKey("boxId"))
-            {
                 // The user is asking/forcing to connect to a different (existing!) box, so we override this
                 cookieBoxId = Request.Query["boxId"];
-            }
-            
-            
 
-            if (string.IsNullOrEmpty( cookieBoxId ))
+
+            if (string.IsNullOrEmpty(cookieBoxId))
             {
                 JObject responseString = await RegisterNewBox();
 
@@ -107,76 +93,47 @@ namespace Hack_The_Crisis.Pages
                 ActivationCode = cookieActivationCode;
                 BoxStatus = (await GetBoxStatus()).GetValue("status").ToString();
             }
-
         }
 
         private async void refreshData(object state)
         {
-            await this.GetBlobUris();
-            await this.GetTexts();
+            await GetBlobUris();
+            await GetTexts();
         }
 
         public async Task<List<string>> GetBlobUris()
         {
             List<string> blobUris = new List<string>();
-            var response = await httpClient.GetAsync("https://codit-htc.azurewebsites.net/api/v1/content/images?boxId=" + BoxId);
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/content/images?boxId=" + BoxId);
             var responseArray = JArray.Parse(await response.Content.ReadAsStringAsync());
-                       
-            foreach (JObject item in responseArray)
-            {
-                blobUris.Add(item.GetValue("mediaUrl").ToString() );
-            }
-           
-                initBlobCount = blobUris.Count;
-            
+
+            foreach (JObject item in responseArray) blobUris.Add(item.GetValue("mediaUrl").ToString());
+
+            initBlobCount = blobUris.Count;
+
             return blobUris;
         }
 
         public async Task<List<string>> GetTexts()
         {
             List<string> texts = new List<string>();
-            var response = await httpClient.GetAsync("https://codit-htc.azurewebsites.net/api/v1/content/messages?boxId=" + BoxId);
+            var response =
+                await HttpClient.GetAsync($"{ApiUrl}/api/v1/content/messages?boxId=" + BoxId);
             var responseArray = JArray.Parse(await response.Content.ReadAsStringAsync());
-           
+
             foreach (JObject item in responseArray)
             {
-
-                DateTime timeStamp = DateTime.Parse(item.GetValue("timestamp").ToString() );
+                DateTime timeStamp = DateTime.Parse(item.GetValue("timestamp").ToString());
                 var dateTimeStr = timeStamp.ToString("HH:mm");
                 var userName = item.GetValue("userName").ToString();
-                var msgTxt = item.GetValue("text").ToString() ;
-                texts.Add("[ "+dateTimeStr+" ] - "+userName +" - "+msgTxt );
-
+                var msgTxt = item.GetValue("text").ToString();
+                texts.Add("[ " + dateTimeStr + " ] - " + userName + " - " + msgTxt);
             }
 
-           
-                initTxtCount = texts.Count;
-            
+            initTxtCount = texts.Count;
+
             return texts;
-        }
-
-     
-
-        public static class JavaScriptConvert
-        {
-            public static HtmlString SerializeObject(object value)
-            {
-                using (var stringWriter = new StringWriter())
-                using (var jsonWriter = new JsonTextWriter(stringWriter))
-                {
-                    var serializer = new JsonSerializer
-                    {
-                        // Let's use camelCasing as is common practice in JavaScript
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    };
-
-                    // We don't want quotes around object names
-                    jsonWriter.QuoteName = false;
-                    serializer.Serialize(jsonWriter, value);
-
-                    return new HtmlString(stringWriter.ToString());
-                }
-            }
         }
     }
 }
